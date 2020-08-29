5f1f5f3d34dbfe00c360ef0a7db7312fc7d0baa5
Manuel Hutter <manuel@hutter.io>
2020-08-29 17:39:25 +0200

    web: Implement filtering by date
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

--- web/src/components/finance/TransactionFilter.js
+++ web/src/components/finance/TransactionFilter.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DateRangePicker } from 'react-dates'
 
-const TransactionFilter = ({ text, setFilter }) => (
-  <p>
-    <input
-      type='text'
-      className='form-control'
-      placeholder='Filter'
-      value={text}
-      onChange={(e) => setFilter({ text: e.target.value })}
-    />
-  </p>
-)
+const TransactionFilter = ({ text, startDate, endDate, setFilter }) => {
+  const [focusedInput, setFocusedInput] = useState('START_DATE')
+  return (
+    <p className='form-row'>
+      <DateRangePicker
+        startDate={startDate}
+        startDateId='journal-date-range-start'
+        startDatePlaceholderText='von'
+        endDate={endDate}
+        endDateId='journal-date-range-end'
+        endDatePlaceholderText='bis'
+        onDatesChange={({ startDate, endDate }) =>
+          setFilter({ text, startDate, endDate })
+        }
+        focusedInput={focusedInput}
+        onFocusChange={setFocusedInput}
+        numberOfMonths={1}
+        showClearDates={true}
+        firstDayOfWeek={1}
+        isOutsideRange={() => false}
+      />
+      <input
+        type='text'
+        className='form-control col-md-8 ml-3 my-auto'
+        placeholder='Filter'
+        value={text}
+        onChange={(e) =>
+          setFilter({ text: e.target.value, startDate, endDate })
+        }
+      />
+    </p>
+  )
+}
 
 export default TransactionFilter
